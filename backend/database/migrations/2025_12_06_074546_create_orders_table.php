53f75a1f5cfbc92adf7b23ff2b144a5f0496d048
zzzsarai <brillantessarahm@gmail.com>
2025-12-09 18:20:06 +0800

    Fixed migrations

--- backend/database/migrations/2025_12_06_074546_create_orders_table.php
+++ backend/database/migrations/2025_12_06_074546_create_orders_table.php
@@ -14,11 +14,17 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null');
+
+            // Add missing customer fields
+            $table->string('customer_name');
+            $table->string('address');
+            $table->string('contact_number');
+            $table->string('payment_method');
+
             $table->decimal('total', 8, 2);
             $table->string('status')->default('pending'); // pending, completed
             $table->timestamps();
         });
-
     }
 
     /**
