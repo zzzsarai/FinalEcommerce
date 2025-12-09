6b68f4388b419b4cddf228c5d01f89615db3bb11
zzzsarai <brillantessarahm@gmail.com>
2025-12-09 16:57:26 +0800

    Added the working backend

--- backend/database/migrations/2025_12_06_101007_create_carts_table.php
+++ backend/database/migrations/2025_12_06_101007_create_carts_table.php
@@ -1,15 +1,11 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-   public function up(): void
+    public function up(): void
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
@@ -19,13 +15,11 @@ return new class extends Migration
             $table->integer('quantity');
             $table->string('image')->nullable();
             $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('carts');
